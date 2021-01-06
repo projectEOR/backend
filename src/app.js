970465dafaf7f3ab77fb5160483cfc6818694075
Graham McLaughlin <grahammclaughlin1324@gmail.com>
2021-01-06 10:42:06 -0500

    added universal pool

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const cors = require('cors')
-
 const express = require('express')
 const app = express()
-
 app.use(cors())
 
 //APIs
@@ -12,9 +10,7 @@ app.use('/reports', reports)
 var bullets = require('./bullets_api')
 app.use('/bullets', bullets)
 
-module.exports = app;
-
 var overview = require('./overview_api')
 app.use('/overview', overview)
 
-
+module.exports = app;
---
+++ src/pool.js
@@ -0,0 +1,11 @@
+//Start and share pool
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'eor',
+    password: 'eor',
+    port: 8000
+});
+
+module.exports = pool;
