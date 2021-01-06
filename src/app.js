7908941a143b085cb6e84dba74f4ac36a5150e00
Graham McLaughlin <grahammclaughlin1324@gmail.com>
2021-01-06 17:45:47 -0500

    reports api complete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 app.use(cors())
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 //APIs
 var reports = require('./reports_api')
