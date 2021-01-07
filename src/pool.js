c874143b2bcdd7303d1869fc253589deee7b93bf
J Silva <72325372+silvasaurus@users.noreply.github.com>
2021-01-07 11:35:32 -0500

    commented out my local db setting on pool.js.

--- src/pool.js
+++ src/pool.js
@@ -1,20 +1,20 @@
 /////////Start and share pool
-// const Pool = require('pg').Pool
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'eor',
-//     password: 'eor',
-//     port: 8000
-// });
-
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "bigbootyjudy",
+  user: "postgres",
   host: "localhost",
-  database: "capstone_db",
-  password: "",
-  port: 5432,
+  database: "eor",
+  password: "eor",
+  port: 8000,
 });
+
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "bigbootyjudy",
+//   host: "localhost",
+//   database: "capstone_db",
+//   password: "",
+//   port: 5432,
+// });
 
 module.exports = pool;
