70a07423cfde5d7107880a9c4f768cfbddeb5522
J Silva <72325372+silvasaurus@users.noreply.github.com>
2021-01-07 11:30:05 -0500

    added post, delete, and put end points. Ops Check good using postman

--- src/tracker_api.js
+++ src/tracker_api.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 const db = require("./tracker_queries");
 
+router.use(express.json());
+
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log("Time: ", Date.now());
@@ -9,8 +11,11 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get("/", db.getTracker);
+router.post("/", db.createTrack);
 router.get("/org/:org_name_id", db.getTrackerByOrg);
 router.get("/rater/:rater_id", db.getTrackerByRater);
 router.get("/ratee/:ratee_id", db.getTrackerByRatee);
+router.delete("/ratee/:ratee_id", db.deleteTrack);
+router.put("/ratee/:ratee_id", db.updateAction);
 
 module.exports = router;
--- src/tracker_queries.js
+++ src/tracker_queries.js
@@ -1,3 +1,4 @@
+const app = require("./app");
 const pool = require("./pool");
 
 const getTracker = (req, res) => {
@@ -51,9 +52,78 @@ const getTrackerByRatee = (req, res) => {
   );
 };
 
+const updateAction = (request, response) => {
+  const ratee_id = parseInt(request.params.ratee_id);
+  const { action_id, member_role } = request.body;
+
+  pool.query(
+    "UPDATE Tracker SET action_id = $1, member_role = $2 WHERE ratee_id = $3",
+    [action_id, member_role, ratee_id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID`);
+    }
+  );
+};
+
+const createTrack = (request, response) => {
+  const {
+    closeout,
+    sq_suspense,
+    gp_suspense,
+    wg_suspense,
+    member_role,
+    ratee_id,
+    rater_id,
+    action_id,
+    org_name_id,
+  } = request.body;
+
+  pool.query(
+    "INSERT INTO Tracker (closeout, sq_suspense, gp_suspense, wg_suspense, member_role, ratee_id, rater_id, action_id, org_name_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+    [
+      closeout,
+      sq_suspense,
+      gp_suspense,
+      wg_suspense,
+      member_role,
+      ratee_id,
+      rater_id,
+      action_id,
+      org_name_id,
+    ],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`record added with ID`);
+    }
+  );
+};
+
+const deleteTrack = (request, response) => {
+  const id = parseInt(request.params.ratee_id);
+
+  pool.query(
+    "DELETE FROM Tracker WHERE ratee_id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`Deleted record with ID`);
+    }
+  );
+};
+
 module.exports = {
   getTracker,
   getTrackerByOrg,
   getTrackerByRater,
   getTrackerByRatee,
+  createTrack,
+  deleteTrack,
+  updateAction,
 };
